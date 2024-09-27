32fb9faa125fbb00518861ee19b0fe1b55e9f60c
jvictormo <jvictormo018@gmail.com>
2024-09-27 15:49:23 -0300

    Adding landing page, and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import './App.css'
-import Funcionários from './assets/páginas/Funcionários'
-import Equipes from './assets/páginas/Equipes'
-import Perguntas from './assets/páginas/Perguntas'
-import SignIn from './assets/páginas/UserSignIn'
-import CadastroAdmin from './assets/páginas/CadastroAdmin'
-import LoginAdmin from './assets/páginas/LoginAdmin'
-import RedefinirSenha from './assets/páginas/RedefinirSenha'
-import CadastroFuncionario from './assets/componentes/CadastroFuncionario'
-import MyCarousel from './assets/componentes/Carrossel'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Funcionários from './assets/páginas/Funcionários';
+import Equipes from './assets/páginas/Equipes';
+import Perguntas from './assets/páginas/Perguntas';
+import CadastroAdmin from './assets/páginas/CadastroAdmin';
+import LoginAdmin from './assets/páginas/LoginAdmin';
+import RedefinirSenha from './assets/páginas/RedefinirSenha';
+import LandPage from './assets/páginas/LandingPage';
 
 function App() {
-
   return (
-      <div>
-       <Funcionários></Funcionários>
-      </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandPage />} />
+        <Route path="/funcionarios" element={<Funcionários />} />
+        <Route path="/equipes" element={<Equipes />} />
+        <Route path="/perguntas" element={<Perguntas />} />
+        <Route path="/cadastro-admin" element={<CadastroAdmin />} />
+        <Route path="/login-admin" element={<LoginAdmin />} />
+        <Route path="/redefinir-senha" element={<RedefinirSenha />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
